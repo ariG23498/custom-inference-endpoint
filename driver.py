55f3adc46b4e94f237cc872d87d32b8535dacc6b
ariG23498 <aritra.born2fly@gmail.com>
2025-11-25 07:50:29 +0100

    chore: add repo, bytes stream, better client

--- driver.py
+++ driver.py
@@ -1,8 +1,12 @@
 import os
+import io
 from transformers import Mistral3ForConditionalGeneration, AutoProcessor
 import torch
 from text_encoding import encode_prompt
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
     text_enc_id = "mistralai/Mistral-Small-3.2-24B-Instruct-2506"
@@ -14,21 +18,25 @@ if __name__ == "__main__":
     tokenizer = AutoProcessor.from_pretrained(tok_id)
 
     prompt = ["hello aritra", "how are you?"]
-    local_prompt_embeds, local_text_ids = encode_prompt(
+    local_prompt_embeds, _ = encode_prompt(
         text_encoder=text_encoder,
         tokenizer=tokenizer,
         prompt=prompt,
     )
     local_prompt_embeds = local_prompt_embeds.detach().cpu()
-    local_text_ids = local_text_ids.detach().cpu()
 
-    url = "https://kcqrahk0dlxhl2zs.us-east-1.aws.endpoints.huggingface.cloud/predict"
+    url = f"{os.environ['ENDPOINT']}/predict"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {os.getenv('HF_TOKEN')}",
+    }
     payload = {
         "prompt": prompt,
     }
-    resp = requests.post(url, json=payload).json()
-    remote_prompt_embeds = torch.tensor(resp["prompt_embeds"], dtype=torch.bfloat16)
-    remote_text_ids = torch.tensor(resp["text_ids"], dtype=torch.int64)
+    resp = requests.post(url, json=payload, headers=headers)
+    resp.raise_for_status()
+
+    remote_prompt_embeds = torch.load(io.BytesIO(resp.content))
 
     torch.testing.assert_close(
         local_prompt_embeds,
@@ -36,4 +44,3 @@ if __name__ == "__main__":
         rtol=1e-3,
         atol=1e-3,
     )
-    torch.testing.assert_close(local_text_ids, remote_text_ids)
